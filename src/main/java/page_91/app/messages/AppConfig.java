f6d7653862bfb511ed066874f61933a79e2b0b6b
hbh <sout1217@gmail.com>
2021-03-06 17:19:52 +0900

    @Component, @Controller, @Service, @Repository 어노테이션을 사용하는 이유와 @Bean의 사용자정의명을 이용하여 context 에서 불러오는 방법

--- src/main/java/page_91/app/messages/AppConfig.java
+++ src/main/java/page_91/app/messages/AppConfig.java
@@ -8,12 +8,17 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("page_91.app.messages")
 public class AppConfig {
 
-    @Bean
+    // @Bean 방식은 간단한 구조를 짤 때 사용되며, 복잡한 구조를 만들때는 비효율적이다
+    // 예를들어 Repository 가 20개 30개 있을 때, 자바코드로 작성하기에는 가독성이 떨어져 한 번에 구조를 이해하는데는 어렵다
+    // 때문에 @Component, @Service, @Controller, @Repository 라는 어노테이션을 사용하여 스프링 컨텍스트가 자동으로 연결을 맺도록 하고 있다
+    // @Component 는 제네럭타입으로 설계되어 해당클래스로 인스턴스화 된다. - 때문에 어떠한 클래스라도 스프링 컨텍스트에서 관리할 수 있다
+
+    @Bean()
     public MessageRepository messageRepository() {
         return new MessageRepository();
     }
 
-    @Bean
+    @Bean("customMessageService")
     MessageService messageService() {
         return new MessageService(messageRepository());
     }
--- src/main/java/page_91/app/messages/Application.java
+++ src/main/java/page_91/app/messages/Application.java
@@ -14,10 +14,15 @@ public class Application {
         // @Bean 어노테이션을 이용하여 스프링 컨텍스트에서 관리 할 수 있게 된다
         MessageService messageService = context.getBean(MessageService.class);
 
+
         // @Bean 으로 스프링 컨텍스트에서 인스턴스들을 관리하고 있기 때문에
         // context 를 이용하여 해당 인스턴스에 접근이 가능하다
         // 아래는 인스턴스화 된 messageService 에 save 메소드를 하여
         // messageService 에서 출력을 하게끔 한다
         messageService.save("Hello, Spring");
+
+        // 사용자가 정의한 @Bean 네임을 이용하여 불러올 수 있다
+        MessageService beanNameMessageService = (MessageService) context.getBean("customMessageService");
+        beanNameMessageService.save("Hell, Bean Name");
     }
 }
